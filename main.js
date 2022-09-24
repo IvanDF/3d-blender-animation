2d0b320da1043d75198d88213a0b9c80d8a97b3a
Ivan Del Fatti <ivandf@MBP-di-Ivan.fritz.box>
2022-09-24 15:14:37 +0200

    3D animation

---
+++ main.js
@@ -0,0 +1,11 @@
+gsap.fromTo(
+  ".hero h2",
+  { opacity: 0, top: "30vh" },
+  { opacity: 1, top: "15vh", duration: 1, delay: 3.2 }
+);
+
+gsap.fromTo(
+  ".hero h3",
+  { opacity: 0, bottom: "25vh" },
+  { opacity: 1, bottom: "5vh", duration: 1, delay: 3.8 }
+);
